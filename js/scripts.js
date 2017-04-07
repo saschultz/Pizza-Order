f26d6f5ddd17c96eaafa3f6f7ba10a06e1064261
Sara Schultz <saschultz95@gmail.com>
2017-04-07 15:31:57 -0700

    add functionaity to display address form in scripts.js on click function and return address with pizza cost.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,16 @@ function Pizza(name, size, toppings) {
   this.name = name;
   this.size = size;
   this.toppings = toppings;
+  this.address = [];
   this.total = 0;
 }
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
 Pizza.prototype.calculateCost = function() {
   if (this.size === "small") {
     this.total = 8;
@@ -25,10 +33,36 @@ Pizza.prototype.calculateCost = function() {
     }
   }
 }
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
 //Front-End Logic:
 $(document).ready(function() {
-  $("#pizza-order").submit(function(e) {
+
+  $("#delivery").click(function() {
+    $("#delivery-form").show();
+    $("#delivery-form").append('<div class="delivery-form">' +
+                              '<h5>Enter the delivery address:</h5>' +
+                             '<div class="form-group">' +
+                               '<label for="new-street">Street</label>' +
+                               '<input type="text" class="form-control new-street" required>' +
+                             '</div>' +
+                             '<div class="form-group">' +
+                               '<label for="new-city">City</label>' +
+                               '<input type="text" class="form-control new-city" required>' +
+                             '</div>' +
+                             '<div class="form-group">' +
+                               '<label for="new-state">State</label>' +
+                               '<input type="text" class="form-control new-state" required>' +
+                             '</div>' +
+                           '</div>');
+      $("#delivery, p").hide();
+    });
+
+  $("form#pizza-order").submit(function(e) {
     e.preventDefault();
+
     var toppingsArray = [];
     var inputSize = $("input:radio[name=size]:checked").val();
     $("input:checkbox[name=toppings]:checked").each(function(){
@@ -36,10 +70,22 @@ $(document).ready(function() {
       toppingsArray.push(inputToppings);
     });
     var inputName = $("input#name").val();
-    console.log(inputName);
+
     var newPizza = new Pizza(inputName, inputSize, toppingsArray);
     newPizza.calculateCost();
 
-    $("#cost").text("Thanks for your order, " + newPizza.name + "! " + "Your total comes to $" + newPizza.total);
+    $(".delivery-form").each(function() {
+
+      var newPizza = new Pizza(inputName, inputSize, toppingsArray);
+      var inputStreet = $(this).find("input.new-street").val();
+      var inputCity = $(this).find("input.new-city").val();
+      var inputState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputStreet, inputCity, inputState)
+      newAddress.fullAddress();
+      newPizza.address.push(newAddress)
+      $("#delivery-return").append("Delivery address: " + newAddress.fullAddress())
+    });
+
+    $("#cost").text("Thanks, " + newPizza.name + "! " + "Your order has been received. " + "Your total will be $" + newPizza.total);
   });
 });
